abac0d17e8ad9613977b3c3d75cbffa8f501e13e
Aşkın Gündüz <91720498+askingunduzz@users.noreply.github.com>
2022-10-09 10:56:45 +0300

    Add files via upload

--- ebebek/Employee.java
+++ ebebek/Employee.java
@@ -1,9 +1,13 @@
 package ebebek;
 
 public class Employee {
+    public static void main(String[] args){
+        Employee emp1 = new Employee("Askin Gunduz", 20000, 45, 1985);
+        emp1.toString();
+    }
+
     String name;
     int salary, workHours, hireYear;
-
     Employee(String name, int salary, int workHours, int hireYear) {
         this.name = name;
         this.salary=salary;
@@ -11,7 +15,7 @@ public class Employee {
         this.hireYear=hireYear;
 
     }
-    double tax;
+    double tax=0;
     public double tax() {
         if(this.salary<1000) {
             return 0;
@@ -21,7 +25,7 @@ public class Employee {
             return tax;
         }
     }
-    int bonus;
+    int bonus=0;
     public int bonus() {
         if(workHours>40){
             bonus=(workHours-40)*30;
@@ -32,43 +36,36 @@ public class Employee {
         }
     }
     double raise;
-    public double raiseSalary() {
-       int year=hireYear-2021;
-       if(year<10){
-           raise=1.05;
-       }
-       else if(year>9&&year<20){
-           raise=1.1;
-       }
-       else{
-           raise=1.15;
-       }
-       return raise;
-    }
 
+    public double raiseSalary() {
+        if (hireYear <= 0 || hireYear > 2021) {
+            System.out.println("Gecersiz bir calisma yili girdiniz, tekrar kontrol ediniz!");
+        }
+        else {
+            int year = hireYear - 2021;
+            if (year < 10) {
+                raise = 1.05;
+            } else if (year > 9 && year < 20) {
+                raise = 1.1;
+            } else {
+                raise = 1.15;
+            }
+            return raise;
+        }
+        return 0.0;
+    }
     public String toString(){
-        double artis=bonus-tax;
-        double raiseartis=artis*raise;
-        double maas_final=salary+raiseartis;
+        double top = salary+ bonus() - tax();
+        double maas_final=top*raiseSalary();
         System.out.println();
         System.out.println("Adi Soyadi: " + name);
         System.out.println("Maasi: " + salary);
         System.out.println("Calisma Saati: " + workHours);
         System.out.println("Baslangic Yili: " + hireYear);
-        System.out.println("Vergi: " + tax());
-        System.out.println("Bonus: " + bonus());
-        System.out.println("Maas Artisi: " + raiseSalary());
+        System.out.println("Maas Artisi: " + (maas_final-salary));
 
         System.out.println("Toplam Maas: " + maas_final);
 
         return null;
     }
-    public static void main(String args) {
-        Employee emp1 = new Employee("Askin Gunduz", 20000, 45, 1985);
-        emp1.toString();
-    }
 }
-
-/*bi de "raiseSalary() hesaplarken vergi ve bonusları dikkate almalısınız." diyo ya yani en son maaşı hesaplarken önce vergili bonuslu maaşı alıp sonra üstüne raiseSalary' yi mi ekleyeceğiz?
-
- */
